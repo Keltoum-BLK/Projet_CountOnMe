939cf823f366fe3c1124df84a1840bd44058af49
Keltoum BELKADI <keltoum.belkadi@gmail.com>
2021-04-19 14:38:35 +0200

    udate viewController

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -13,27 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet var numberButtons: [UIButton]!
     
     var simpleCalc = SimpleCalc()
-
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-
-    // Error check computed variables
-    var expressionIsCorrect: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/"
-    }
-
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-
-    var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "x" && elements.last != "/"
-    }
-
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
     
     var alreadyReset : Bool {
         return textView.text != ""
